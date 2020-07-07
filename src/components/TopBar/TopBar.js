78f4987dc980b31e8304ec781d86952a4ce85872
Dmitry <45153082+TheAlterBlack@users.noreply.github.com>
2020-07-08 00:06:50 +0300

    Top bar (#1)
    * Initial commit
    
    * Delete .idea folder
    
    * Added TopBar
    
    * Delete build folder
    
    * Small fixes

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class TopBar extends Component {
+    render() {
+        return (
+            <header className='header-desktop'>
+                <div className='section__content section__content--p30'>
+                    <div className='container-fluid'>
+                        <div className='header-wrap'/>
+                    </div>
+                </div>
+            </header>
+        );
+    };
+}
---
+++ src/configureStore.js
@@ -0,0 +1,14 @@
+/*import {
+    applyMiddleware,
+    createStore,
+} from 'redux';
+import thunk from 'redux-thunk';
+import createRootReducer from './reducers';
+
+export default createStore(
+    createRootReducer(),
+    {},
+    applyMiddleware(
+        thunk,
+    ),
+);*/
