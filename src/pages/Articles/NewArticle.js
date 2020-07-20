e96519604a3ada7c82695aec984b7d31d575e28a
TheAlterBlack <alterblackoffical@gmail.com>
2020-07-20 18:13:02 +0300

    Adding file fix

--- src/pages/Articles/NewArticle.js
+++ src/pages/Articles/NewArticle.js
@@ -1,28 +1,25 @@
 import React, { PureComponent } from "react";
 import { Form, Field } from 'react-final-form';
 
-export default class NewArticle extends PureComponent{
-    onSubmit = (values, form) => {
-        console.log(values);
+export default class NewArticle extends PureComponent {
+    onSubmit = (values) => {
+        let formData = new FormData();
 
-        let formData = new FormData(form);
-        formData.set('title', values.title);
+        formData.append('title', values.title);
         formData.append('metaDescription', values.metaDescription);
         formData.append('text', values.text);
-        formData.append('image', values.image);
-
-        console.log(formData);
+        formData.append('image', values.image[0]);
 
         this.props.createArticle(formData);
     };
 
     render() {
-        const { loading } = this.props;
+        const {loading} = this.props;
 
         return (
-            <Form onSubmit={ this.onSubmit }
-                  render={({ handleSubmit, values, form}) => (
-                      <form onSubmit={ handleSubmit }>
+            <Form onSubmit={this.onSubmit}
+                  render={({handleSubmit, values}) => (
+                      <form onSubmit={handleSubmit}>
                           <div className="form-group">
                               <label>Название</label>
                               <Field className="au-input au-input--full"
@@ -41,26 +38,26 @@ export default class NewArticle extends PureComponent{
                           </div>
                           <div className="form-group">
                               <label>Текст статьи</label>
-                              <Field name="image">
-                                  {({ input: { value, onChange, ...input } }) => {
-                                      const handleChange = ({ target }) => {
-                                          onChange(target.files) // instead of the default target.value
-                                      }
-                                      return <input {...input} type="file" onChange={handleChange} />
-                                  }}
-                              </Field>
+                              <Field className="au-input au-input--full"
+                                     name="text"
+                                     type="text"
+                                     component="input"/>
                           </div>
                           <div className="form-group">
                               <label>Картинка статьи</label>
-                              <Field className="au-input au-input--full"
-                                     name="image"
-                                     type="file"
-                                     component="input"/>
+                              <Field name="image">
+                                  {({input: {value, onChange, ...input}}) => {
+                                      const handleChange = ({target}) => {
+                                          onChange(target.files) // instead of the default target.value
+                                      }
+                                      return <input {...input} type="file" onChange={handleChange}/>
+                                  }}
+                              </Field>
                           </div>
 
                           <button className="au-btn au-btn--block au-btn--green m-b-20"
                                   type="submit"
-                                  disabled={ loading }>Добавить
+                                  disabled={loading}>Добавить
                           </button>
                       </form>
                   )}
